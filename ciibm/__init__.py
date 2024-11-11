6df1c408878774628ff7629e7347410b3b6daea2
Gregory Hering <Gregory.Hering@ibm.com>
2024-11-11 12:49:27 -0800

    new version 0.1.1

--- ciibm/__init__.py
+++ ciibm/__init__.py
@@ -8,3 +8,4 @@ from .datacenter import GET_GW_ID
 from .datacenter import ASSOCIATE_AND_ROUTE_THROUGH_VLAN
 
 __all__ = ['cliibm_auth','GET_DATACENTER_LOCATION','GET_BACKEND_ROUTER','ORDER_VLAN','GET_VLAN_ID','ORDER_SUBNET_CREATOR','GET_GW_ID','ASSOCIATE_AND_ROUTE_THROUGH_VLAN']
+__version__ = "0.1.1"
